c968ae8db0388e352f188df5d15b6b93ee07585f
proxima424 <darpitrangari42@gmail.com>
2023-06-12 06:47:15 +0530

    wowwwwww yessssss fuckkkkkk

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# PoolMaxi-contracts
+# Pool-Maxi V1 (No-Loss Lottery Protocol with Higher Yields)
+</br>
+
+![Contract Architechture](explain.png)
+
+</br>
+
+## Lottery Game with No Loss Criteria - A Financially Significant Opportunity
+
+This repository introduces a Pool Together inspired lottery game that offers participants a unique and financially significant opportunity. Unlike traditional lotteries, our protocol ensures that every participant experiences no  financial loss. This innovative concept opens up exciting possibilities for users to engage in a lottery-like experience while preserving their initial investment.
+
+Yield source being based on the high-APR proven sources like the LP tokens of GMX, QuickPerps, etc.
+it's way way better than the rewards PoolTogether provides.
+The objective is to create a "no-loss-lottery" environment where participants can deposit their LP tokens and earn rewards collectively.
+
+### How it works
+1. Deposits : Users call the `depositQLP` function of the ongoing contest. They initially have to give approval to this smart contract of the LP tokens being dealt with.
+2. Reward Collection : After the `depositDeadline` gets passed, the protocol calls `handleRewards` function of the `RewardRouter` contract and receives the collective amount of trading fees being allocated to the amount of LP tokens the Contest contract holds.
+3. Winner Selection : `DirectFundingConsumer` model of fetching random number from `Chainlink VRF` is used to generate onchain randomness. This randomness is used to select the winner and set the `winnerAddress`
+4. User Token Return : After the `withdrawDeadline` has passed, each user can then call `reclaimReward()` function and gets back whatever amount he had deposited. More better, if this user is the randomly chosen winner, he gets transferred all the trading fees reward as his fucking prize.
+
+### Structurized Components
+1. Factory Contracts : The Factory contracts are responsible for deploying multiple instances of Contest contracts. Each Contest contract represents a unique lottery game session.
+
+2. Contest Contracts : The Contest contracts handle the execution and management of individual lottery game sessions. Once a session is completed, a new Contest contract is deployed by the Factory contract to continue the lottery experience seamlessly.
+
+Until next time, [@proxima424](https://twitter.com/proxima424)
+
+
+
+
+
+
+
+
---
+++ src/PMfactory.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {PMcontest} from "./PMcontest.sol";
+import {IContest} from "./IContest.sol";
+
+contract PMfactory {
+    /*//////////////////////////////////////////////////////////////
+                              ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error PM_notOwner(address actor);
+    error PM_zeroAddress();
+    error PM_contestDoesNotExist(uint256 id);
+    error PM_settingWinnerFailed(uint256 timestamp);
+
+    /*//////////////////////////////////////////////////////////////
+                              EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event PM_ownerChanged(address indexed oldOwner, address indexed newOwner, uint256 time);
+
+    /*//////////////////////////////////////////////////////////////
+                              STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    address public owner;
+    uint256 public contestCount;
+    mapping(uint256=>bool) isContestRunning;
+    mapping(uint256=>address) contestAddress;
+
+    /*//////////////////////////////////////////////////////////////
+                              MODIFIER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    modifier onlyOwner() {
+        checkOwner(msg.sender);
+        _;
+    }
+
+    function checkOwner(address _address) private {
+        if (_address != owner) {
+            revert PM_notOwner(_address);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              OWNER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function changeOwner(address _address) external onlyOwner {
+        if (_address == address(0)) {
+            revert PM_zeroAddress();
+        }
+        address oldOwner = owner;
+        assembly {
+            sstore(owner.slot, _address)
+        }
+        emit PM_ownerChanged(oldOwner, _address, block.timestamp);
+    }
+
+    function deployNewContest(
+        uint256 _depositDeadline,
+        address _fsQLPaddress,
+        uint256 _withdrawStart,
+        address _rewardRouter,
+        address _wethAddress
+    ) external onlyOwner returns (address newContest,uint256 id) {
+        newContest = address(new PMcontest(_depositDeadline,_fsQLPaddress,_withdrawStart,_rewardRouter,_wethAddress));
+        id = contestCount++;
+        isContestRunning[id] = true;
+        contestAddress[id] = newContest;
+    }
+
+    function contestSetWinner(uint256 _id) onlyOwner external returns(address winner){
+        if(contestAddress[_id]==address(0)){
+            revert PM_contestDoesNotExist(_id);
+        }
+
+        winner = IContest(contestAddress[_id]).setWinner();
+
+        if(winner == address(0)){
+            revert PM_settingWinnerFailed(block.timestamp);
+        }
+
+
+    }
+
+    function redeemContestAwards(uint256 _id) onlyOwner external returns(uint256 winningAmount){
+        if(contestAddress[_id]==address(0)){
+            revert PM_contestDoesNotExist(_id);
+        }
+        winningAmount = IContest(contestAddress[_id]).PM_redeemReward();
+        isContestRunning[_id] = false;
+    }
+
+    function getWinnerAddress(uint256 _id) external returns(address){
+        return IContest(contestAddress[_id]).winnerAddress();
+    }
+
+
+
+
+
+
+}
